14ffcf2868bd66045d588c77936a06761d059ad9
自分の姓名（ローマ字） <takuma.ride@gmail.com>
2025-06-25 22:38:07 +0900

    ログアウト後の遷移先をログイン画面に変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
-   def after_sign_in_path_for(resource)
+  # ログイン後：マイページへ
+  def after_sign_in_path_for(resource)
     users_mypage_path
+  end
+
+  # ログアウト後：ログイン画面へ
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
   end
 end
