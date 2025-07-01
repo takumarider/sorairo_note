7234fe422e2b7f2c8a931141b8c37d4b37c19208
自分の姓名（ローマ字） <takuma.ride@gmail.com>
2025-07-01 23:27:02 +0900

    時間枠削除機能のバグ修正とビュー修正

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,14 +1,15 @@
 # app/controllers/admin/base_controller.rb
-
 class Admin::BaseController < ApplicationController
-  before_action :authenticate_user!
-  before_action :ensure_admin!
+  before_action :authenticate_user!   # Deviseでログイン済みか？
+  before_action :authenticate_admin!  # adminユーザーか？
+
+  layout "admin"  # 管理者用のレイアウトを適用
 
   private
 
-  def ensure_admin!
+  def authenticate_admin!
     unless current_user&.admin?
-      redirect_to root_path, alert: "権限がありません"
+      redirect_to root_path, alert: "管理者専用ページです。"
     end
   end
 end
--- app/controllers/admin/slots_controller.rb
+++ app/controllers/admin/slots_controller.rb
@@ -1,6 +1,48 @@
 # app/controllers/admin/slots_controller.rb
 class Admin::SlotsController < Admin::BaseController
+  before_action :set_slot, only: %i[edit update destroy]
+
   def index
-    @slots = Slot.all
+    @slots = Slot.order(:start_time)
   end
+
+  def new
+    @slot = Slot.new
+  end
+
+  def create
+    @slot = Slot.new(slot_params)
+    if @slot.save
+      redirect_to admin_slots_path, notice: "時間枠を作成しました"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @slot = Slot.find(params[:id])
+  end
+
+  def update
+    if @slot.update(slot_params)
+      redirect_to admin_slots_path, notice: "時間枠を更新しました"
+    else
+      render :edit
+    end
+  end
+
+    def destroy
+      @slot.destroy
+      redirect_to admin_slots_path, notice: "時間枠を削除しました"
+    end
+
+  private
+
+  def set_slot
+   @slot = Slot.find(params[:id])
+   end
+
+   def slot_params
+   params.require(:slot).permit(:start_time, :end_time, :available)
+   end
 end
